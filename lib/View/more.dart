cafc21ef1bb82b0d4eea87d18cd63b972a7a3922
sun4035 <sun403546@naver.com>
2023-01-09 23:54:32 +0900

    Firebase 연동 - more 추가

---
+++ lib/View/more.dart
@@ -0,0 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class More extends StatelessWidget {
+  const More({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        FloatingActionButton(onPressed: (){
+          FirebaseFirestore.instance
+              .collection('member')
+              .doc('nnPXotopD2Rk5YKoDrWv')
+              .get()
+              .then((value) => print(value['id']));
+        },),
+        SafeArea(child: Text('더보기')),
+        // Text('ㄱㄱ')
+      ],
+    );
+  }
+}
